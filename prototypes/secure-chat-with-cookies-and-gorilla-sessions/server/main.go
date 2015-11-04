1bb5ed351123365b3e2750487ed019ada415d454
William van der Kamp <william.vanderkamp@gmail.com>
2015-11-03 18:22:54 -0600

    got gorilla-session-based chat working. Huzzah. Only rudimentary manual testing.

--- prototypes/secure-chat-with-cookies-and-gorilla-sessions/server/main.go
+++ prototypes/secure-chat-with-cookies-and-gorilla-sessions/server/main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
-	"crypto/rand"
-	"encoding/base64"
-	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
-	"golang.org/x/net/websocket"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
-	"sync"
 	"time"
 )
 
 const (
-	SESSION_DURATION_MINUTES int = 30
+	SESSION_DURATION_MINUTES int    = 30
+	SESSION_NAME             string = "chat-session"
+	HTTP_PARAM_USERNAME      string = "username"
+	SESSION_KEY_USERNAME     string = HTTP_PARAM_USERNAME
 )
 
 var activeSessions = sessions.NewCookieStore([]byte("a-secret-key-i-guess"))
 var chat *Chat
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
 
 type Chat struct {
 	join     chan *websocket.Conn
@@ -71,7 +75,7 @@ func (chat *Chat) Leave(client *websocket.Conn) {
 
 func (chat *Chat) Broadcast(message *Message) {
 	for _, client := range chat.clients {
-		websocket.JSON.Send(client, message)
+		client.WriteJSON(message)
 	}
 	log.Println("broadcasted message")
 }
@@ -80,36 +84,6 @@ type Message struct {
 	Username string    `json:"username"`
 	Time     time.Time `json:"time"`
 	Message  string    `json:"message"`
-}
-
-func wsHandler(ws *websocket.Conn) {
-	existingSession, err := activeSessions.Get(r, session.Username)
-	if err != nil {
-		http.Error(w, err.Error(), 500)
-		return
-	}
-
-	//TODO inform the user of invalidity
-	if existingSession.IsNew() {
-		log.Println("active session not found.")
-		existingSession.Options = &activeSessions.Options{MaxAge: -1}
-		existingSession.Save(r, w)
-		return
-	}
-
-	receiver := NewReceiver()
-	chat.join <- ws
-	var message Message
-	for receiver.receive(ws, &message) {
-		message.Username = session.Username
-		message.Time = time.Now()
-		chat.incoming <- &message
-	}
-	if receiver.err != nil {
-		log.Println(receiver.err)
-	}
-
-	chat.leave <- ws
 }
 
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
@@ -127,7 +101,7 @@ func NewReceiver() *Receiver {
 }
 
 func (r *Receiver) receive(ws *websocket.Conn, v interface{}) bool {
-	r.err = websocket.JSON.Receive(ws, v)
+	r.err = ws.ReadJSON(v)
 	return r.err == nil
 }
 
@@ -143,32 +117,96 @@ func NewAuthHandler() *AuthHandler {
 
 func (a *AuthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Println("serving http request")
-	// if direct access is needed, use this syntax.
-	// r.URL.Query().Get("username")
-	session, err := activeSessions.Get(r, "session")
+
+	session, err := activeSessions.Get(r, SESSION_NAME)
+	if err != nil {
+		log.Println(err.Error())
+		// http.Error(w, err.Error(), 500)
+		// return
+	}
+
+	log.Printf("number of values already in new session: %d.\n", len(session.Values))
+	userName := r.URL.Query().Get(HTTP_PARAM_USERNAME)
+	if userName == "" {
+		log.Println("nonexistent username in authentication request.")
+		session.Options = &sessions.Options{MaxAge: -1}
+		session.Save(r, w)
+		return
+	}
+
+	session.Values[SESSION_KEY_USERNAME] = userName
+	session.Save(r, w)
+}
+
+type WsHandler struct {
+	err error
+}
+
+func NewWsHandler() *WsHandler {
+	return &WsHandler{
+		err: nil,
+	}
+}
+
+// serveWs handles websocket requests from the peer.
+func (wsh *WsHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+
+	existingSession, err := activeSessions.Get(r, SESSION_NAME)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	log.Println("number of values already in new session: " + len(session.Values))
-	session.Save(r, w)
+	//TODO inform the user of invalidity
+	if existingSession.IsNew {
+		log.Println("active session not found.")
+		existingSession.Options = &sessions.Options{MaxAge: -1}
+		existingSession.Save(r, w)
+		return
+	}
+
+	existingSession.Save(r, w)
+
+	ws, err := upgrader.Upgrade(w, r, w.Header())
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	receiver := NewReceiver()
+	chat.join <- ws
+	var message Message
+	for receiver.receive(ws, &message) {
+		message.Username = existingSession.Values[SESSION_KEY_USERNAME].(string)
+		message.Time = time.Now()
+		chat.incoming <- &message
+	}
+	if receiver.err != nil {
+		log.Println(receiver.err)
+	}
+
+	chat.leave <- ws
 }
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	activeSessions.Options{
+	activeSessions.Options = &sessions.Options{
 		Path:     "/",
 		MaxAge:   60 * SESSION_DURATION_MINUTES,
 		HttpOnly: true,
+		Secure:   true,
 	}
 
 	chat = NewChat()
 
 	r := mux.NewRouter()
 
-	r.Handle("/ws", websocket.Handler(wsHandler))
+	r.Handle("/ws", NewWsHandler())
 	r.Handle("/auth", NewAuthHandler())
 	r.PathPrefix("/").Handler(http.FileServer(http.Dir("app/")))
 
