77fa87a44d46b4d9ac27af33cc859c97292be270
Ken S <okenso@gmail.com>
2015-11-12 16:35:02 -0600

    finished up the basic logic for purchases and paying a member
    need to work the database stuff into this, as well as advanced logic
    (sliders) and any other TODO i missed.

--- prototypes/logic/main.go
+++ prototypes/logic/main.go
@@ -6,55 +6,75 @@ import (
 )
 
 type user struct {
-	userName string
+	userName       string
 	expected, owed int
 }
 
-
 //TODO will need to work out how to divy up existing debt
-func addMember(s *[]*user, x *user) []*user {
-	*s = append(*s, x)
-	return *s
+func addMember(group *[]*user, x *user) []*user {
+	*group = append(*group, x)
+	return *group
 }
-
 
 //TODO work out how to spread the remainder of his money around
 //MaybeTODO an error if the user is not in the group.
-func removeMember(s *[]*user, x *user) []*user {
-	t := make([]*user, len(*s)-1) // can't use the append cut trick with *s
-	t = *s
-	for i,ele := range *s {
-  		if *ele == *x {
-  			t = append(t[:i], t[i+1:]...)
-  			break
-  		}
+func removeMember(group *[]*user, x *user) []*user {
+	t := make([]*user, len(*group)-1) // can't use the append cut trick with *s
+	t = *group
+	for i, ele := range *group {
+		if *ele == *x {
+			t = append(t[:i], t[i+1:]...)
+			break
+		}
 	}
 	//fmt.Println("t: ", t)
-	*s = t
-	return *s
+	*group = t
+	return *group
 }
 
-func printGroup(s *[]*user) {
-	t := make([]*user, len(*s)-1)
-	t = *s
- 	//output := []string {""}
-		for i := range *s {
-			fmt.Printf(" %v ", t[i]) // use %+v for struct vals, %p for pointer
- 	}
- 	fmt.Println("")
+func printGroup(group *[]*user) {
+	t := make([]*user, len(*group)-1)
+	t = *group
+	//output := []string {""}
+	for i := range *group {
+		fmt.Printf(" %v ", t[i]) // use %+v for struct vals, %p for pointer
+	}
+	fmt.Println("")
 }
 
+// Adds a purchase for the buyer, increasing the expeced by (cost-average)
+// lowers all other group members expected by average
+func addPurchase(group *[]*user, buyer *user, cost int) []*user {
+	var length int = (len(*group) - 1)
+	var average int = (cost / length)
+	for _, ele := range *group {
+		if *ele == *buyer {
+			ele.owed = ele.owed + cost
+			ele.expected = ele.expected + (cost - average)
+		} else {
+			ele.expected = ele.expected - average
+		}
+	}
+	return *group
+}
+
+func payMember(payer *user, payee *user, amount int) {
+	payer.expected += amount
+	payer.owed += amount
+	payee.expected -= amount
+	payee.owed -= amount
+}
 
 func main() {
 	fmt.Println("My favorite number is swag")
 	K := user{"Ken", 0, 0}
 	W := user{"Will", 0, 0}
 	J := user{"Josh", 0, 0}
-	E := user{"Evan", 0 ,0}
-	X := user{"Jordan", 0 ,0}
+	E := user{"Evan", 0, 0}
+	X := user{"Jordan", 0, 0}
 	fmt.Println("Users: ", K, W, J, E)
 	//Group := make([]user,0)
-	Group := []*user {&K, &W, &J, &E}
+	Group := []*user{&K, &W, &J, &E}
 	//Group = append(Group, K, W, J, E)
 	//fmt.Println("Group = ", Group)
 	fmt.Println("Group Creation")
@@ -68,10 +88,16 @@ func main() {
 	removeMember(&Group, &X)
 	printGroup(&Group) //fmt.Println("Removed Group = ", Group)
 
-	fmt.Println("Group Modify Values Directly")
-	Group[1].expected = 10
-	E.expected = 15
+	//fmt.Println("Group Modify Values Directly")
+	//Group[1].expected = 10
+	//E.expected = 15
+	//printGroup(&Group)
+
+	fmt.Println("Add purchase for K")
+	addPurchase(&Group, &K, 90)
 	printGroup(&Group)
 
+	fmt.Println("J pay K 5")
+	payMember(&J, &K, 5)
+	printGroup(&Group)
 }
-
