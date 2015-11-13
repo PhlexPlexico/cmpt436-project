20f7e19973ab9b607596365287da7730fd111ecd
William van der Kamp <william.vanderkamp@gmail.com>
2015-11-13 16:31:03 -0600

    almost have things working, except my sessions aren't being saved properly.

---
+++ prototypes/secure-chat-auth/server/auth.go
@@ -0,0 +1,264 @@
+/*
+This is just gothic, adapted by William van der Kamp to include the provider in the session store,
+so that provider.FetchUser() can be called immediately, rather than going through
+the whole login process.
+
+See https://github.com/markbates/goth/blob/master/gothic/gothic.go for the source.
+*/
+
+/*
+Package gothic wraps common behaviour when using Goth. This makes it quick, and easy, to get up
+and running with Goth. Of course, if you want complete control over how things flow, in regards
+to the authentication process, feel free and use Goth directly.
+See https://github.com/markbates/goth/examples/main.go to see this in action.
+*/
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/sessions"
+	"github.com/markbates/goth"
+)
+
+// SessionName is the key used to access the session store.
+const (
+	SessionName                     = "_gothic_session"
+	PROVIDER_NAME_KEY               = "gothic_provider"
+	USER_KEY                        = "gothic_user"
+	SESSION_SECRET_CONFIG_FILE_PATH = "../../../.session_secret"
+)
+
+func marshalUser(user *goth.User) (string, error) {
+	b, err := json.Marshal(user)
+	return string(b), err
+}
+
+func unmarshalUser(data string) (*goth.User, error) {
+	var user *goth.User
+	err := json.Unmarshal([]byte(data), user)
+	return user, err
+}
+
+func getProviderNameFromSession(s *sessions.Session) (string, error) {
+	val := s.Values[PROVIDER_NAME_KEY]
+	if val == nil {
+		return "", errors.New("provider not stored in session")
+	}
+	return val.(string), nil
+}
+
+func getProviderFromSession(s *sessions.Session) (goth.Provider, error) {
+	providerName, err := getProviderNameFromSession(s)
+	if err != nil {
+		return nil, err
+	}
+	return goth.GetProvider(providerName)
+}
+
+func getUserFromSession(s *sessions.Session) (*goth.User, error) {
+	val := s.Values[USER_KEY]
+	if val == nil {
+		return nil, errors.New("user not stored in session")
+	}
+	userString := val.(string)
+	return unmarshalUser(userString)
+}
+
+/*
+ * Does not save the session.
+ */
+func putProviderInSession(provider goth.Provider, s *sessions.Session) {
+	s.Values[PROVIDER_NAME_KEY] = provider.Name()
+}
+
+/*
+ * Does not save the session.
+ */
+func putUserInSession(user *goth.User, s *sessions.Session) error {
+	userString, err := marshalUser(user)
+	if err != nil {
+		return err
+	}
+
+	s.Values[USER_KEY] = userString
+	return nil
+}
+
+// Store can/should be set by applications using gothic. The default is a cookie store.
+var Store *sessions.CookieStore
+var defaultStore *sessions.CookieStore
+
+var keySet = false
+
+func init() {
+	//Changed by William: use a config file instead of an environment variable
+	key, err := ioutil.ReadFile(SESSION_SECRET_CONFIG_FILE_PATH)
+	if err != nil {
+		log.Println("could not load session secret from file ",
+			SESSION_SECRET_CONFIG_FILE_PATH)
+		log.Println(err)
+	}
+	keySet = len(key) != 0
+	Store = sessions.NewCookieStore([]byte(key))
+	Store.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   60 * SESSION_DURATION_MINUTES,
+		HttpOnly: true,
+		Secure:   true,
+	}
+	defaultStore = Store
+}
+
+/*
+BeginAuthHandler is a convienence handler for starting the authentication process.
+It expects to be able to get the name of the provider from the query parameters
+as either "provider" or ":provider".
+BeginAuthHandler will redirect the user to the appropriate authentication end-point
+for the requested provider.
+See https://github.com/markbates/goth/examples/main.go to see this in action.
+*/
+func BeginAuthHandler(res http.ResponseWriter, req *http.Request) {
+	url, err := GetAuthURL(res, req)
+	if err != nil {
+		res.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(res, err)
+		return
+	}
+
+	http.Redirect(res, req, url, http.StatusTemporaryRedirect)
+}
+
+// GetState gets the state string associated with the given request
+// This state is sent to the provider and can be retrieved during the
+// callback.
+var GetState = func(req *http.Request) string {
+	return "state"
+}
+
+/*
+GetAuthURL starts the authentication process with the requested provided.
+It will return a URL that should be used to send users to.
+It expects to be able to get the name of the provider from the query parameters
+as either "provider" or ":provider".
+I would recommend using the BeginAuthHandler instead of doing all of these steps
+yourself, but that's entirely up to you.
+*/
+func GetAuthURL(res http.ResponseWriter, req *http.Request) (string, error) {
+
+	if !keySet && defaultStore == Store {
+		fmt.Println("William says: the following error should never occur!")
+		fmt.Println("goth/gothic: no SESSION_SECRET environment variable is set. The default cookie store is not available and any calls will fail. Ignore this warning if you are using a different store.")
+	}
+
+	providerName, err := GetProviderName(req)
+	if err != nil {
+		return "", err
+	}
+
+	provider, err := goth.GetProvider(providerName)
+	if err != nil {
+		return "", err
+	}
+	sess, err := provider.BeginAuth(GetState(req))
+	if err != nil {
+		return "", err
+	}
+
+	url, err := sess.GetAuthURL()
+	if err != nil {
+		return "", err
+	}
+
+	session, _ := Store.Get(req, SessionName)
+
+	//added by William: store both the provider and the session.
+	session.Values[SessionName] = sess.Marshal()
+	session.Values[PROVIDER_NAME_KEY] = provider.Name()
+	err = session.Save(req, res)
+	if err != nil {
+		return "", err
+	}
+
+	return url, err
+}
+
+/*
+CompleteUserAuth does what it says on the tin. It completes the authentication
+process and fetches all of the basic information about the user from the provider.
+It expects to be able to get the name of the provider from the query parameters
+as either "provider" or ":provider".
+See https://github.com/markbates/goth/examples/main.go to see this in action.
+*/
+var CompleteUserAuth = func(res http.ResponseWriter, req *http.Request) (goth.User, error) {
+
+	if !keySet && defaultStore == Store {
+		fmt.Println("William says: the following error should never occur!")
+		fmt.Println("goth/gothic: no SESSION_SECRET environment variable is set. The default cookie store is not available and any calls will fail. Ignore this warning if you are using a different store.")
+	}
+
+	providerName, err := GetProviderName(req)
+	if err != nil {
+		return goth.User{}, err
+	}
+
+	provider, err := goth.GetProvider(providerName)
+	if err != nil {
+		return goth.User{}, err
+	}
+
+	session, _ := Store.Get(req, SessionName)
+
+	if session.Values[SessionName] == nil {
+		return goth.User{}, errors.New("could not find a matching session for this request")
+	}
+
+	sess, err := provider.UnmarshalSession(session.Values[SessionName].(string))
+	if err != nil {
+		return goth.User{}, err
+	}
+
+	_, err = sess.Authorize(provider, req.URL.Query())
+
+	if err != nil {
+		return goth.User{}, err
+	}
+
+	return provider.FetchUser(sess)
+}
+
+// GetProviderName is a function used to get the name of a provider
+// for a given request. By default, this provider is fetched from
+// the URL query string. If you provide it in a different way,
+// assign your own function to this variable that returns the provider
+// name for your request.
+var GetProviderName = getProviderName
+
+func getProviderName(req *http.Request) (string, error) {
+	log.Println("getting provider name")
+	provider := req.URL.Query().Get("provider")
+	if provider == "" {
+		provider = req.URL.Query().Get(":provider")
+	}
+	if provider == "" {
+		session, err := Store.Get(req, SessionName)
+		if err != nil {
+			return provider, err
+		}
+
+		provider, err = getProviderNameFromSession(session)
+		if err != nil {
+			return provider, err
+		}
+	}
+	if provider == "" {
+		return provider, errors.New("you must select a provider")
+	}
+
+	return provider, nil
+}
--- prototypes/secure-chat-auth/server/main.go
+++ prototypes/secure-chat-auth/server/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gorilla/sessions"
 	"github.com/gorilla/websocket"
 	"github.com/markbates/goth"
-	"github.com/markbates/goth/gothic"
 	"github.com/markbates/goth/providers/gplus"
 	"golang.org/x/oauth2/google"
 	"html/template"
@@ -19,8 +18,7 @@ import (
 const (
 	SESSION_DURATION_MINUTES       int    = 30
 	SESSION_NAME                   string = "chat-session"
-	HTTP_PARAM_USERNAME                   = "username"
-	SESSION_KEY_USERNAME                  = HTTP_PARAM_USERNAME
+	SESSION_KEY_USERNAME                  = "username"
 	GOOGLE_CLIENT_SECRET_FILE_PATH        = "../../../.gplus_client_secret.json"
 )
 
@@ -30,8 +28,6 @@ var upgrader = websocket.Upgrader{
 }
 
 var router *pat.Router
-
-var activeSessions = sessions.NewCookieStore([]byte("a-secret-key-i-guess"))
 
 func redirectHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "https://localhost:8080"+r.RequestURI, http.StatusMovedPermanently)
@@ -52,31 +48,95 @@ func (r *Receiver) receive(ws *websocket.Conn, v interface{}) bool {
 	return r.err == nil
 }
 
+func endSession(s *sessions.Session, w http.ResponseWriter, r *http.Request) {
+	s.Options = &sessions.Options{MaxAge: -1}
+	s.Save(r, w)
+}
+
+/**
+ * return true if a pre-existing user was attached, false otherwise.
+ * if false, an associated error may be returned (unless there simply was no
+ * pre-existing user.
+ */
+func tryToAttachPreexistingUserToSession(
+	w http.ResponseWriter, r *http.Request) (bool, error) {
+	session, err := Store.Get(r, SESSION_NAME)
+	if err != nil {
+		log.Println("unable to get session.")
+		return false, err
+	}
+
+	if session.IsNew {
+		return false, nil
+	}
+
+	provider, err := getProviderFromSession(session)
+	if err != nil {
+		log.Println("unable to get provider")
+		return false, err
+	}
+
+	sess, err := provider.UnmarshalSession(session.Values[SessionName].(string))
+	if err != nil {
+		log.Println("unable to unmarshal sess from session")
+		endSession(session, w, r)
+		return false, err
+	}
+
+	user, err := provider.FetchUser(sess)
+	if err != nil {
+		log.Println("unable to fetch user with ", provider.Name())
+		endSession(session, w, r)
+		return false, err
+	}
+
+	err = putUserInSession(&user, session)
+	if err != nil {
+		log.Println("unable to store user in session.")
+		endSession(session, w, r)
+		return false, err
+	}
+
+	session.Save(r, w)
+	return true, nil
+}
+
 func AuthChoiceHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("serving auth request")
 
-	t, err := template.New("foo").Parse(indexTemplate)
+	wasUserFound, err := tryToAttachPreexistingUserToSession(w, r)
 	if err != nil {
-		fmt.Fprintln(w, err)
-		return
+		log.Println(err)
 	}
 
-	t.Execute(w, nil)
+	if !wasUserFound {
+		log.Println("serving new login.")
+		t, err := template.New("foo").Parse(indexTemplate)
+		if err != nil {
+			fmt.Fprintln(w, err)
+			return
+		}
+
+		t.Execute(w, nil)
+	}
 }
 
 func AuthCallbackHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("serving auth callback")
 
 	//TODO make use of more user attributes, besides name.
-	user, err := gothic.CompleteUserAuth(w, r)
+	user, err := CompleteUserAuth(w, r)
 	if err != nil {
 		fmt.Fprintln(w, err)
 		return
 	}
 
-	session, err := activeSessions.Get(r, SESSION_NAME)
+	session, err := Store.Get(r, SESSION_NAME)
+
 	if err != nil {
 		log.Println(err.Error())
+		//Apparently this err does not mean no session was created, so I don't need
+		//to return.
 		// http.Error(w, err.Error(), 500)
 		// return
 	}
@@ -86,38 +146,54 @@ func AuthCallbackHandler(w http.ResponseWriter, r *http.Request) {
 	// 	fmt.Fprintln(w, err)
 	// 	return
 	// }
-
 	// t.Execute(w, user)
 
 	log.Printf("number of values already in new session: %d.\n", len(session.Values))
 
-	session.Values[SESSION_KEY_USERNAME] = user.Name
-	session.Save(r, w)
+	err = putUserInSession(&user, session)
+	if err != nil {
+		http.Error(w, "unable to store user in session", 500)
+		endSession(session, w, r)
+		return
+	}
 
-	http.ServeFile(w, r, "app/")
+	session.Save(r, w)
+	log.Println("163")
+	// http.ServeFile(w, r, "app/")
+	http.Redirect(w, r, "/realapp", http.StatusMovedPermanently)
 }
 
 // serveWs handles websocket requests from the peer.
 func WsHandler(w http.ResponseWriter, r *http.Request) {
-	existingSession, err := activeSessions.Get(r, SESSION_NAME)
+	session, err := Store.Get(r, SESSION_NAME)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
-
+	log.Println("175")
 	//TODO inform the user of invalidity
-	if existingSession.IsNew {
-		log.Println("active session not found.")
-		existingSession.Options = &sessions.Options{MaxAge: -1}
-		existingSession.Save(r, w)
+	if session.IsNew {
+		http.Error(w, "invalid action: user is not logged in.", 401)
+		endSession(session, w, r)
 		return
 	}
 
-	existingSession.Save(r, w)
+	//TODO try commenting this out, see if it makes a difference (it shouldn't)
+	session.Save(r, w)
+
+	user, err := getUserFromSession(session)
+	if err != nil {
+		log.Println("188")
+		http.Error(w, "unable to retrieve user from session", 500)
+		endSession(session, w, r)
+		return
+	}
+	log.Println("192")
+	userName := user.Name
 
 	ws, err := upgrader.Upgrade(w, r, w.Header())
 	if err != nil {
-		log.Println(err)
+		http.Error(w, err.Error(), 500)
 		return
 	}
 
@@ -125,7 +201,7 @@ func WsHandler(w http.ResponseWriter, r *http.Request) {
 	chat.join <- ws
 	var message Message
 	for receiver.receive(ws, &message) {
-		message.Username = existingSession.Values[SESSION_KEY_USERNAME].(string)
+		message.Username = userName
 		message.Time = time.Now()
 		chat.incoming <- &message
 	}
@@ -147,26 +223,25 @@ func main() {
 	//TODO do I need scopes?
 	// https://developers.google.com/+/domains/authentication/scopes
 	googleConfig, err := google.ConfigFromJSON(jsonKey)
+
+	//gives me "profile", "email", "openid" scopes by default.
 	goth.UseProviders(
 		gplus.New(googleConfig.ClientID, googleConfig.ClientSecret,
 			googleConfig.RedirectURL),
 	)
-
-	activeSessions.Options = &sessions.Options{
-		Path:     "/",
-		MaxAge:   60 * SESSION_DURATION_MINUTES,
-		HttpOnly: true,
-		Secure:   true,
-	}
 
 	chat = NewChat()
 	router = pat.New()
 
 	router.Get("/ws", WsHandler)
 	router.Get("/app", AuthCallbackHandler)
-	router.Get("/auth/{provider}", gothic.BeginAuthHandler)
+	router.Get("/auth/{provider}", BeginAuthHandler)
 	router.Get("/", AuthChoiceHandler)
+	// router.Add("GET", "/realapp", http.FileServer(http.Dir("app/")))
+	// router.PathPrefix("/realapp").Handler(http.FileServer(http.Dir("app/")))
 	http.Handle("/", router)
+	http.Handle("/realapp/", http.StripPrefix("/realapp/",
+		http.FileServer(http.Dir("app/"))))
 
 	go http.ListenAndServeTLS(":8080", "cert.crt", "key.key", nil)
 	http.ListenAndServe(":8000", http.HandlerFunc(redirectHandler))
