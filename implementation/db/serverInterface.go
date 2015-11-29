51fd0be0a504f3da076fcaef67817504762257ed
William van der Kamp <william.vanderkamp@gmail.com>
2015-11-29 13:17:07 -0600

    added documentation to server interface stubs

--- implementation/db/serverInterface.go
+++ implementation/db/serverInterface.go
@@ -14,6 +14,9 @@ const (
 	feedItemTypePayment      string = "payment"
 )
 
+/*
+ * All inbound websocket messages take this form.
+ */
 type FeedItem struct {
 	// The actual feed item to be unmarshaled, based upon the type.
 	Content json.RawMessage `json:"content"`
@@ -24,11 +27,18 @@ type FeedItem struct {
 	Type string `json:"type"`
 }
 
+/* For debug purposes. */
 func (fi *FeedItem) String() string {
-
 	return fmt.Sprint(fi.GroupId, ":", fi.Type, ":", string(fi.Content))
 }
 
+/*
+ * Handle all inbound websocket messages. This is where all payments and purchases
+ * are given to the back-end, to be processed appropriately. There is no need for
+ * this handle function to return a value to the webserver, because the webserver
+ * rebroadcasts incoming feed items on its own, automatically. Just return an error
+ * if the webserver should not be rebroadcasting: e.g. if an invalid purchase is made.
+ */
 func HandleFeedItem(fi *FeedItem) error {
 	switch fi.Type {
 	case feedItemTypeComment:
@@ -41,6 +51,8 @@ func HandleFeedItem(fi *FeedItem) error {
 		 * doing DB insertions, etc. There is no need to rebroadcast the
 		 * new feed item, as the webserver handles that automatically.
 		 */
+
+		/* The rest of the cases can be done exactly like the comment case. */
 	case feedItemTypeNotification:
 	case feedItemTypePayment:
 	case feedItemTypePurchase:
@@ -51,35 +63,67 @@ func HandleFeedItem(fi *FeedItem) error {
 	return nil
 }
 
+/*
+ * NOTE: the following stubs usually pass in and ask for strings instead of
+ * bson.ObjectIDs. I think these can just be type-converted directly. I just
+ * worked with strings in my code to... reduce coupling, I guess?
+ */
+
 /**
  * creates a new user if necessary, and returns a string representation of
  * the user's id.
+ * If the error is not nil, the returned value must be ignored.
  */
 func GetUserIdString(email string) (string, error) {
 	return "", nil
 }
 
+/*
+ * Get all groups associated with this user.
+ * If the error is not nil, the returned value must be ignored.
+ */
 func GetGroups(userId string) ([]Group, error) {
 	return nil, nil
 }
 
-/* Get the user corresponding to each userId. */
+/*
+ * Get the user corresponding to each userId.
+ * If the error is not nil, the returned value must be ignored.
+ */
 func GetUsers(userIds []bson.ObjectId) ([]User, error) {
 	return nil, nil
 }
 
+/*
+ * Get all the groupIds associated with this userId.
+ * If the error is not nil, the returned value must be ignored.
+ */
 func GetGroupIdStrings(userId string) ([]string, error) {
 	return nil, nil
 }
 
+/*
+ * Add the user with the given userId to the group with the given groupId.
+ * If the error is not nil, the returned value must be ignored.
+ */
 func AddUserToGroup(userId, groupId string) error {
 	return nil
 }
 
+/*
+ * Create a group with the given group name, and with the given users as
+ * members. Return the new group's ID, in string form.
+ * If the error is not nil, the returned value must be ignored.
+ */
 func CreateGroup(name string, userIds []string) (string, error) {
 	return "", nil
 }
 
+/*
+ * Add a contact with the given email to the given user's list of contacts.
+ * Return the new Contact object.
+ * If the error is not nil, the returned value must be ignored.
+ */
 func AddContact(userId string, contactEmail string) (Contact, error) {
 	return Contact{}, nil
 }
