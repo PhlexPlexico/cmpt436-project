7bf7009b37bcf4423200c8024d8c460104523e42
Ken S <okenso@gmail.com>
2015-12-07 20:48:49 -0600

    feeds are in db if they're from the web site?
    have to call the function in the test to insert them tho

--- implementation/main/main.go
+++ implementation/main/main.go
@@ -3,15 +3,26 @@ package main
 import (
 	"../db"
 	"../server"
+	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	//Start the backend before starting the server that relies upon it.
-	//(William doesn't know how to get the back-end to work, so he's commented
-	// it out.)
-	db.Init()
-	//This is a blocking call. It just serves forever.
-	server.Serve()
+
+	if os.Args[1] == "test" {
+		fmt.Println(len(os.Args), os.Args[len(os.Args)-1])
+
+		db.Test()
+	} else {
+		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		//Start the backend before starting the server that relies upon it.
+		//(William doesn't know how to get the back-end to work, so he's commented
+		// it out.)
+		db.Init()
+		//This is a blocking call. It just serves forever.
+		server.Serve()
+
+	}
+
 }
